be65e253fbdc38a681fbe4dcee71e0ca72144546
iws-seed <iwstechnical7@gmail.com>
2026-02-03 16:28:29 +0100

    added the 3 files of output parsers

---
+++ src/2.0_output_formatting/2.2-basic_structured_output/3_enums_control_values.ts
@@ -0,0 +1,12 @@
+import { generateText, Output } from 'ai';
+import { google } from '@ai-sdk/google';
+import { z } from 'zod';
+
+const model = google('gemini-1.5-flash');
+
+async function classifySentiment() {
+
+}
+
+
+classifySentiment();
