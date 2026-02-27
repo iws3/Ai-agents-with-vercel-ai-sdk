0909869c9f881c31e1fb35b13c4a75b159dcf317
iws-seed <iwstechnical7@gmail.com>
2026-02-27 18:51:38 +0100

    executed multiple tools

--- src/3.0_tool_calling/1_anatomy_of_tools.ts
+++ src/3.0_tool_calling/1_anatomy_of_tools.ts
@@ -9,7 +9,7 @@ const model=google("gemini-2.5-flash");
 
 const weatherTool=tool({
     description:"Get the current weather for a given location",
-    parameters:z.object({
+    inputSchema:z.object({
     location:z.string().describe('The city and country, eg. "Paris, France"')
     }),
     execute:async ({location})=>{
@@ -25,24 +25,4 @@ const weatherTool=tool({
     }
 })
 
-// HOW TOOL WITH GENERATE TEXT:
-// When you provide tools to generateText, something interesting happens during execution. The AI processes your prompt and decides it needs weather information. It doesn't just generate text saying "I don't know". Instead, it generates a tool call specifying which tool to use and what parameters to pass. The SDK sees this tool call, finds your execute function, runs it with the provided parameters, and sends the result back to the AI. The AI then generates its final response incorporating the real weather data.
 
-async function askAboutWeather(){
-    const result=await generateText({
-        model:model,
-        tools: {
-            getWeather:weatherTool
-        },
-        prompt: 'what is the weather in Douala-Cameroon right now?'
-    })
-}
-
-// TOOL 2: SEARCH for restaurants
-
-const searchRestaurantsTool=tool({
-    description:"Search for restaurant in a given location",
-    parameters:z.object({
-        location:z.string().describe('')
-    })
-})
